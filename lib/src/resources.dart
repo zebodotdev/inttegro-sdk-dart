508ca1d80c1ba4a0076e83024c39bea20d0e2e1d
Yaw Boakye at Zebo <yaw@zebo.dev>
2026-09-09 00:13:03 +0100

    dart: apply package language formatting

--- lib/src/resources.dart
+++ lib/src/resources.dart
@@ -490,14 +490,15 @@ final class Files {
   Future<FileDownload> contents(
     FileContentsRequest request, {
     RequestOptions options = const RequestOptions(),
-  }) => _client._download(
-    "POST",
-    "/files/contents",
-    request.toJson(),
-    options,
-    "files.contents",
-    authenticated: true,
-  );
+  }) =>
+      _client._download(
+        "POST",
+        "/files/contents",
+        request.toJson(),
+        options,
+        "files.contents",
+        authenticated: true,
+      );
 
   /// Delete a file
   Future<File> delete(
